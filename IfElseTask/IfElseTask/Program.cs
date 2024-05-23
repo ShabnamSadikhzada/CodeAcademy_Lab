75ca87bebbc6eb7e62014cd434f1b249d0c24fdf
Shabnam J. Sadikhzada <shabnamjs@code.local>
2024-05-23 13:35:36 +0400

    added switch case task

---
+++ IfElseTask/IfElseTask/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace IfElseTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            EvenOrOdd(num);
+            PositiveOrNegative(num);
+
+            int gpa = 99;
+            CheckGPA(gpa);
+        }
+
+        public static void EvenOrOdd(int num)
+        {
+            if (num <= 0)
+            {
+                Console.WriteLine("your num is not even or odd");
+                return;
+            }
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("your num is even");
+            }
+            else
+            {
+                Console.WriteLine("your num is odd");
+            }
+        }
+        
+        public static void PositiveOrNegative(int num) 
+        {
+            if(num == 0)
+            {
+                Console.WriteLine("your num is not positive or negative");
+                return;
+            }
+            if (num > 0)
+            {
+                Console.WriteLine("your num is positive");
+            }
+            else
+            {
+                Console.WriteLine("your num is negative");
+            }
+        }
+
+        public static void CheckGPA(int gpa)
+        {
+            if (gpa >= 91) 
+            {
+                Console.WriteLine("elaci");
+            }
+            else
+            {
+                Console.WriteLine("elaci doul");
+            }
+        }
+    }
+}
---
+++ SwitchCaseTask/SwitchCaseTask/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SwitchCaseTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthTranslater();
+            MonthTranslater1();
+        }
+
+        public static void MonthTranslater()
+        {
+            string season = Console.ReadLine().ToLower();
+
+            switch (season)
+            {
+                case "spring":
+                    season = "yaz";
+                    break;
+                case "winter":
+                    season = "qış";
+                    break;
+                case "autumn":
+                    season = "payız";
+                    break;
+                case "summer":
+                    season = "yay";
+                    break;
+                default:
+                    season = "invalid season name";
+                    break;
+            }
+
+            Console.WriteLine(season);
+        }
+
+        public static void MonthTranslater1()
+        {
+            string season = Console.ReadLine().ToLower();
+
+            string seasonVal = season switch
+            {
+                "spring" => "yaz",
+                "winter" => "qis",
+                "autumn" => "payiz",
+                "summer" => "yay",
+                _ => "invalid season name"
+            };
+
+            Console.WriteLine(seasonVal);
+        }
+    }
+}
