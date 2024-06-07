cd6c47aadf2429d1a80d5bf3bdbde70f7218ba3c
Shabnam J. Sadikhzada <shabnamjs@code.local>
2024-06-07 13:08:48 +0400

    added oop example

---
+++ OOPExample/OOPExample/Models/Book.cs
@@ -0,0 +1,100 @@
+﻿namespace Example.Models;
+
+public class Library
+{
+    public List<Book> Books = new List<Book>();
+    private static int _id = 1;
+
+    public void AddBook()
+    {
+        Book book = new Book();
+
+        book.Id = _id;
+        _id++;
+
+        Console.WriteLine("enter book name:");
+        book.Name = Console.ReadLine();
+
+        Console.WriteLine("enter author name:");
+        book.AuthorName = Console.ReadLine();
+
+        Console.WriteLine("enter book page count:");
+        book.PageCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("enter book price:");
+        book.Price = Convert.ToDouble(Console.ReadLine());
+
+        Books.Add(book);
+    }
+
+    public Book GetBookById(int id)
+    {
+        foreach (var book in Books)
+        {
+            if (book.Id == id)
+            {
+                return book;
+            }
+        }
+
+        return null;
+    }
+
+    public int FindAllBooks(string authorName)
+    {
+        int count = 0;
+        foreach (var book in Books)
+        {
+            if (book.AuthorName == authorName)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public List<Book> RemoveAllBooks(int code)
+    {
+        List<Book> RemovedBooks = new List<Book>();
+        Book removedBook = GetBookById(code);
+        RemovedBooks.Add(removedBook);
+        Books.Remove(removedBook);
+
+        return RemovedBooks;
+    }
+}
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string AuthorName { get; set; }
+    public int PageCount { get; set; }
+    public double Price { get; set; }
+}
+
+public class Order
+{
+    private static int _id { get; set; }
+    public List<Book> Books { get; set; }
+    public double Price { get; set; }
+    public DateTime Date { get; set; }
+
+    public void Satish(int id, Library lib)
+    {
+        foreach (var book in lib.Books)
+        {
+            if (book.Id == id)
+            {
+                Books.Add(book);
+                Price += book.Price;
+            }
+            else
+            {
+                Console.WriteLine("book with ID {0} was not found.", id);
+            }
+        }
+
+        _id++;
+    }
+}
---
+++ OOPExample/OOPExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using Example.Models;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            #region book adding
+            int isCountinue = 0;
+
+            Console.WriteLine("enter book details. if you want add another book enter 1, else enter 0");
+            while (true)
+            {
+                isCountinue = Convert.ToInt32(Console.ReadLine());
+                if (isCountinue == 1)
+                {
+                    library.AddBook();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            #endregion
+
+            library.FindAllBooks("TTTTostoyevski");
+
+            Order order = new Order();
+
+            #region order adding
+            int isCountinue2 = 0;
+            int id;
+            Console.WriteLine("enter book id. if you want add another book enter 1, else enter 0");
+            while (true)
+            {
+                isCountinue = Convert.ToInt32(Console.ReadLine());
+                if (isCountinue == 1)
+                {
+                    id = Convert.ToInt32(Console.ReadLine());
+                    order.Satish(id, library);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            #endregion
+        }
+    }
+}
